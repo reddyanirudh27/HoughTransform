49e7eb0f006e666ea4403a01edfb1dd719efe417
reddyanirudh27 <reddy.anirudh27@gmail.com>
2020-07-15 21:54:51 -0400

    wip

--- hough_lines_draw.py
+++ hough_lines_draw.py
@@ -1,23 +1,8 @@
 import math
 import cv2 as cv2
-import numpy as np
 
 
 def hough_lines_draw(img,  peaks, rho, theta):
-    # Draw lines found in an image using Hough transform.
-    #
-    # img: Image on top of which to draw lines
-    # outfile: Output image filename to save plot as
-    # peaks: Qx2 matrix containing row, column indices of the Q peaks found in accumulator
-    # rho: Vector of rho values, in pixels
-    # theta: Vector of theta values, in degrees
-
-    # TODO: Your code here
-
-    # imgout = np.zeros([img.shape[0], img.shape[1], 3])
-    # imgout[:, :, 0] = img
-    # imgout[:, :, 1] = img
-    # imgout[:, :, 2] = img
 
     for peak in peaks:
         d0, angle = rho[peak[0]], theta[peak[1]]
--- hough_peaks.py
+++ hough_peaks.py
@@ -2,22 +2,6 @@ import numpy as np
 
 
 def hough_peaks(H, Npeaks=1, Threshold=0, NHoodSize=[0, 0]):
-    # Find peaks in a Hough accumulator array.
-    #
-    # Threshold (optional): Threshold at which values of H
-    #   are considered to be peaks
-    # NHoodSize (optional): Size of the suppression neighborhood, [M N]
-    #
-    # Please see the Matlab documentation for houghpeaks():
-    # http://www.mathworks.com/help/images/ref/houghpeaks.html
-    # Your code should imitate the matlab implementation.
-
-    # Parse input arguments
-    # addOptional(p, 'numpeaks', 1, @isnumeric);
-    # addParameter(p, 'Threshold', 0.5 * max(H(:)));
-    # addParameter(p, 'NHoodSize', floor(size(H) / 100.0) * 2 + 1)
-    # odd values >= size(H)/50
-    # parse(p, varargin{:});
 
     # TODO: Your code here
     if(Threshold == 0):
